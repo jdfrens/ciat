73bff462cf35055c5848c06efa34571eeba6c836
Mark Van Holstyn <mvanholstyn@gmail.com>
2008-07-29 00:00:45 -0400

    Don't need to create the module (it has already been created).

--- lib/ciat.rb
+++ lib/ciat.rb
@@ -1,12 +1,8 @@
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
+# Loads in the basic files needed to use CIAT.  Compilers and executors have to be required separately.
+# CIAT::Base is the class you really want to look at.
 require 'ciat/base'
 require 'ciat/test'
 require 'ciat/filenames'
-
-# Loads in the basic files needed to use CIAT.  Compilers and executors have to be required separately.
-# CIAT::Base is the class you really want to look at.
-module CIAT
-  
-end

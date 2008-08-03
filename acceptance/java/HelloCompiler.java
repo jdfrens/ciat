fc528c1b2a1095871b05153076fc2a9abd3a7b1f
Jeremy D. Frens <jdfrens@gmail.com>
2008-08-03 16:52:49 -0400

    Working on acceptance tests.

---
+++ acceptance/java/HelloCompiler.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class HelloCompiler {
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(args[0]));
+        String input = reader.readLine();
+        reader.close();
+        PrintWriter printer = new PrintWriter(args[1]);
+        printer.println("Hello, " + input + ".\n");
+        printer.close();
+        System.exit(0);
+    }
+    
+}

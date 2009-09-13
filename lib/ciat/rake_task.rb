aaad714b5ddbe7d52b7cd03fa7b4cc6876fd79bf
Jeremy D. Frens <jdfrens@gmail.com>
2009-09-12 20:12:26 -0500

    adding to documentation

--- lib/ciat/rake_task.rb
+++ lib/ciat/rake_task.rb
@@ -3,20 +3,21 @@
 # This is the class to use in your +Rakefile+.  The simplest use looks like
 # this:
 #
-#   CIAT::RakeTask do |t|
+#   CIAT::RakeTask(:name_of_task) do |t|
 #     t.processors << compiler
 #     t.processors << executor
 #   end
 #
 # Define +compiler+ and +executor+ in the +Rakefile+ to return a compiler and
-# executor, respectively.  The options on +t+ (the task in this example)
-# correspond directly to the options of CIAT::Suite.  See CIAT::Suite for 
-# defaults and details.
+# executor, respectively.  <code>:name_of_task</code> is (as I try to make
+# clear by its name) the name of the rake task.  If you leave out this name,
+# it defaults to <code>:ciat</code>.  The options on +t+ correspond directly
+# to the options of CIAT::Suite.  See CIAT::Suite for defaults and details.
 #
 # * <code>t.processors</code> is <em>required</em> and specifies the
-#   processors to be executed; order matters!  Use <code>=</code> to assign
-#   a list of processors; use <code>&lt;&lt;</code> to push processors onto
-#   the existing list of processors.
+#   processors to be executed; order matters!  Use <code>=</code> to assign a
+#   list of processors; use <code>&lt;&lt;</code> to push processors onto the
+#   existing list of processors.
 # * <code>t.folder</code> specifies folders to search for test files.
 # * <code>t.files</code> is an array of specific files.
 # * <code>t.report_filename</code> is the name of the report

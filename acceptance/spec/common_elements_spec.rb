170f6cdf2766af07f573bd4fbf41ff313221641c
Jeremy D. Frens <jdfrens@gmail.com>
2009-11-18 19:23:55 -0600

    refactored common-element acceptance spec

--- acceptance/spec/common_elements_spec.rb
+++ acceptance/spec/common_elements_spec.rb
@@ -5,31 +5,27 @@ require 'spec_helper'
 describe "confirm the common elements in the HTML documents" do
   include Webrat::Matchers
   
-  def all_html_files
-    Dir["temp/java.html"]
-  end
-  
-  before(:each) do
-    @docs = all_html_files.map { |file| webrat_document(file) }
-  end
-  
-  it "should include prototype javascript" do
-    each_doc_should have_selector("script",
-                    :src => "prototype.js",  :type => "text/javascript")
-  end
-  
-  it "should include a link to a CSS file" do
-    each_doc_should have_selector("link", :rel => "stylesheet",
-                    :href => "ciat.css", :type => "text/css")
-  end
-  
-  it "should include a test-count span" do
-    each_doc_should have_selector("span.test-count")
-  end
-  
-  def each_doc_should(matcher)
-    @docs.each do |doc|
-      doc.should matcher
+  Dir["temp/*.html"].each do |file|
+    describe file do
+      define_method(:file) { file }
+      
+      def this_doc
+        @cached_doc ||= Nokogiri::HTML.parse(open(file).read)
+      end
+      
+      it "should include prototype javascript" do
+        this_doc.should have_selector("script",
+                      :src => "prototype.js",  :type => "text/javascript")
+      end
+
+      it "should include a link to a CSS file" do
+        this_doc.should have_selector("link", :rel => "stylesheet",
+                        :href => "ciat.css", :type => "text/css")
+      end
+
+      it "should include a test-count span" do
+        this_doc.should have_selector("span.test-count")
+      end
     end
   end
 end
